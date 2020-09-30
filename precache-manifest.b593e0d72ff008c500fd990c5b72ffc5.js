91b3173d0be40afa8d2de8540a1d66fd51f6ac2e
fuseter <phongkhanet.ka.61@ubu.ac.th>
2020-09-30 10:12:53 +0700

    Updates

---
+++ precache-manifest.b593e0d72ff008c500fd990c5b72ffc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a912047f5a561247d21323809b0d748",
+    "url": "/CommathProject/index.html"
+  },
+  {
+    "revision": "b3459eee0136e6a24ad9",
+    "url": "/CommathProject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dfb8e2b99dc01d7a16c",
+    "url": "/CommathProject/static/js/2.5feb24cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CommathProject/static/js/2.5feb24cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3459eee0136e6a24ad9",
+    "url": "/CommathProject/static/js/main.e9ff6175.chunk.js"
+  },
+  {
+    "revision": "15592fde08b0f7960a2c",
+    "url": "/CommathProject/static/js/runtime-main.4411668f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CommathProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
